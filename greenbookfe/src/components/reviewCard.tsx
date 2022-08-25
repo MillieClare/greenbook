8009b1336c57921b533e08ae30fa13131edffea5
Millie Clare <Millie_Clare@outlook.com>
2022-08-26 00:27:24 +0100

    so I THOUGHT I added it but I didn't - this is the text formatting for nice looking cards

--- greenbookfe/src/components/reviewCard.tsx
+++ greenbookfe/src/components/reviewCard.tsx
@@ -1,9 +1,23 @@
-import { Card, CardContent, Typography, Grid, Button } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Button,
+  Paper,
+  Tooltip,
+} from "@mui/material";
 import CountryText from "./countryText";
 import { FontFamilies } from "../styles/fonts/fontFamilies";
-import { FileDownload } from "@mui/icons-material";
+import { FileDownload, FileDownloadOff } from "@mui/icons-material";
 import { Colors } from "./../styles/colors";
 import CompanyRating from "./companyRating";
+import { styled } from "@mui/material/styles";
+import { getCountryFlag } from "../utilities/countryFormat";
+
+const Item = styled(Grid)(({ theme }) => ({
+  padding: theme.spacing(1),
+}));
 
 const ReviewCard = ({
   link,
@@ -21,124 +35,163 @@ const ReviewCard = ({
   rating?: number;
 }) => {
   return (
-    <Grid item xs={12} md={4}>
-      <Card
-        elevation={0}
-        sx={{
-          width: "100%",
-          height: { xs: "100%", md: 125 },
+    <Grid
+      item
+      xs={12}
+      sm={6}
+      md={4}
+      style={{ borderRadius: 10, overflow: "hidden" }}
+    >
+      <Grid
+        container
+        spacing={2}
+        style={{
           backgroundColor: "rgb(92, 219, 149,0.25)",
-          borderRadius: 2.5,
-          transition: "all 250ms ease-in",
+          borderRadius: 10,
+          overflow: "hidden",
         }}
       >
-        <CardContent>
-          <Grid
-            container
-            rowSpacing={1}
-            direction="row"
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            justifyContent="space-between"
-            alignItems="flex-start"
-          >
-            <Grid
-              item
-              xs={12}
-              md={8}
-              justifyContent="flex-start"
-              alignItems="center"
-              container
-              direction="row"
-              textOverflow={"ellipsis"}
-              zeroMinWidth
-            >
-              <Typography
-                textOverflow={"ellipsis"}
-                noWrap
-                gutterBottom
-                variant="body1"
-                component="div"
-                fontFamily={FontFamilies.Montserrat}
-                sx={{ paddingBottom: 0, marginBottom: 0, fontWeight: "400" }}
-              >
-                {company}
-              </Typography>
-              <Typography
-                gutterBottom
-                variant="caption"
-                component="div"
-                fontFamily={FontFamilies.Montserrat}
-                sx={{ paddingBottom: 0, marginBottom: 0 }}
-              >
-                {sector}
-              </Typography>
-              <Typography
-                gutterBottom
-                variant="caption"
-                component="div"
-                fontFamily={FontFamilies.Montserrat}
-                sx={{ paddingBottom: 0, marginBottom: 0 }}
-              >
-                {reviewer}
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={4}
-              justifyContent={{ xs: "flex-start ", md: "flex-end" }}
-              alignItems="flex-start"
-              container
-              direction="row"
-            >
-              {country ? <CountryText country={country} /> : null}
-            </Grid>
-          </Grid>
-
-          <Grid
-            container
-            direction="row"
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-            justifyContent="space-between"
+        <Grid item xs={12} lg={7}>
+          <Item
+            style={{ height: 80 }}
+            item
+            justifyContent="flex-start"
             alignItems="center"
           >
-            <Grid
-              item
-              xs={12}
-              md={7}
-              justifyContent={{ xs: "center ", md: "flex-start" }}
-              alignItems="center"
-              style={{ display: "flex" }}
+            <Typography
+              textOverflow={"ellipsis"}
+              noWrap
+              gutterBottom
+              variant="body1"
+              component="div"
+              fontFamily={FontFamilies.Montserrat}
+              sx={{ paddingBottom: 0, marginBottom: 0, fontWeight: "400" }}
             >
-              {rating ? <CompanyRating rating={rating} /> : null}
-            </Grid>
-            <Grid
-              item
-              xs={12}
-              md={5}
-              justifyContent={{ xs: "center ", md: "flex-end" }}
-              alignItems="center"
-              style={{ display: "flex" }}
-            >
-              {link ? (
-                <Button
-                  variant="contained"
-                  endIcon={<FileDownload />}
-                  disableElevation
-                  style={{
-                    fontFamily: FontFamilies.Montserrat,
-                    backgroundColor: Colors.green,
-                    color: "#FFFFFF",
-                    fontWeight: "bold",
-                  }}
+              {company}
+            </Typography>
+            <Grid container direction="row">
+              <Grid
+                item
+                display={{ xs: "start", sm: "start", md: "start", lg: "none" }}
+              >
+                <Tooltip title={country as string}>
+                  <div style={{ paddingRight: 10 }}>
+                    {getCountryFlag(country)}
+                  </div>
+                </Tooltip>
+              </Grid>
+              <Grid item>
+                <Typography
+                  gutterBottom
+                  variant="caption"
+                  component="div"
+                  fontFamily={FontFamilies.Montserrat}
+                  sx={{ paddingBottom: 0, marginBottom: 0 }}
                 >
-                  Download
-                </Button>
-              ) : null}
+                  {sector}
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
+            <Typography
+              gutterBottom
+              variant="caption"
+              component="div"
+              fontFamily={FontFamilies.Montserrat}
+              sx={{ paddingBottom: 0, marginBottom: 0 }}
+            >
+              {reviewer}
+            </Typography>
+          </Item>
+        </Grid>
+        <Grid
+          item
+          xs={0}
+          lg={5}
+          style={{ height: 80 }}
+          display={{ xs: "none", sm: "none", md: "none", lg: "flex" }}
+          alignContent={"flex-end"}
+        >
+          <Item
+            style={{ height: 80, width: "100%" }}
+            item
+            display={"flex"}
+            justifyContent="flex-end"
+            alignContent="flex-end"
+            alignItems="flex-start"
+            textAlign={"right"}
+          >
+            <Grid
+              container
+              direction="row"
+              justifyContent="flex-end"
+              alignContent="flex-end"
+              alignItems="flex-start"
+              textAlign={"right"}
+            >
+              <div style={{ paddingRight: 10 }}>{getCountryFlag(country)}</div>
+              <Typography
+                variant="caption"
+                component="div"
+                fontFamily={FontFamilies.Montserrat}
+                sx={{ paddingBottom: 0, marginBottom: 0 }}
+              >
+                {country}
+              </Typography>
+            </Grid>
+          </Item>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6}>
+          <Item item justifyContent="flex-start" alignItems="center">
+            {rating ? <CompanyRating rating={rating} /> : null}
+          </Item>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          lg={6}
+          display={"flex"}
+          alignContent={"center"}
+          alignItems={"center"}
+          style={{ padding: 16 }}
+        >
+          {link ? (
+            <Button
+              variant="contained"
+              endIcon={<FileDownload />}
+              disableElevation
+              fullWidth
+              style={{
+                fontFamily: FontFamilies.Montserrat,
+                backgroundColor: Colors.green,
+                color: "#FFFFFF",
+                fontWeight: "bold",
+                margin: 0,
+              }}
+            >
+              Download
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              endIcon={<FileDownloadOff />}
+              disableElevation
+              disabled={true}
+              fullWidth
+              style={{
+                fontFamily: FontFamilies.Montserrat,
+                backgroundColor: Colors.darkGreen,
+                color: "#FFFFFF",
+                fontWeight: "bold",
+                margin: 0,
+              }}
+            >
+              Unavailable
+            </Button>
+          )}
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
