1d95895cbd138e24a210250e5427bfcbe2188daa
Millie Clare <Millie_Clare@outlook.com>
2022-09-10 21:54:58 +0100

    added react router

--- greenbookfe/src/index.tsx
+++ greenbookfe/src/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
@@ -11,9 +13,11 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
